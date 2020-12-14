d601c6aa4a88cd52f51db9c92192b46223a73e22
Koichi MATSUMOTO <mzch@mac.com>
2020-12-15 07:10:09 +0900

    fix mistake of defaullt value
    fix mistake of default value

--- config.default.js
+++ config.default.js
@@ -2,9 +2,9 @@ module.exports = {
   port: 4500, 
   redis: {
     // url: 'redis://127.0.0.1:6379/0',
-    host: '192.168.207.201',
+    host: '127.0.0.1',
     port: 6379,
-    db: 7,
+    db: 0,
     session: 'sessions'
   }
 }
